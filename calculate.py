089fabc2e56ad37e29bd614c0742991eee3b6dd9
Eous <aleksandarhough@protonmail.com>
2020-06-15 14:04:57 +0200

    first commit

---
+++ calculate.py
@@ -0,0 +1,57 @@
+import math
+import sys
+
+default = input('Do you want to use default for FoV? (60° horisontal Fov, 10° vertica and 100m range) (y/n) ')
+
+if default == 'y':
+    h_deg = 60
+    v_deg = 10
+    rang = 100
+elif default == 'n':
+    h_deg = int(input('enter horisontal FoV in degrees: ')) #how much deg the drone can see horisontally
+    v_deg = int(input('enter vertical FoV in degrees: '))#how much deg the drone can see horisontally
+    rang = int(input('enter range: '))
+else:
+    sys.exit('invalid input')
+
+rh = h_deg / 2 
+rv = v_deg / 2 
+
+def convert(degrees):
+    radians = degrees * 0.0174533
+    return radians
+
+cos_r = convert(rh) #calculates cos in radians
+
+if default =='y':
+    custom_h = 'n'
+else:
+    custom_h = input('do you want to use a max height? (y/n) ')
+
+# h = heigt
+
+if custom_h == 'n':
+    h = int(input('input the custom height: '))
+elif custom_h == 'y':
+    h =math.cos(cos_r) * rang 
+else:
+    sys.exit('invalid input')
+
+
+print('maximum usable height = {0}m'.format(h))
+
+#rg = ground half-radius
+#Rg = ground radius
+tan_r = convert(rh)
+rg = math.tan(tan_r) * h
+Rg = rg *2
+
+tan_r2 = convert(rv)
+rg2 = math.tan(tan_r2) * h
+Rg2 = rg2 * 2
+
+print('Horisontal FoV radius= {}'.format(Rg))
+print('Vertical Fov radius = {}'.format(Rg2))
+
+Fov = Rg * Rg2
+print('Visible surface= {}m'.format(Fov))
